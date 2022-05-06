beaabbd49bf0c23c39212c004f7783fe2de4e022
ThanushiPerera <88557471+it20639112Thanu@users.noreply.github.com>
2022-05-06 23:15:49 +0530

    UpdateStuudentRegister.java

--- app/src/main/java/com/example/macqurarieinterns/StudentRegisterActivity.java
+++ app/src/main/java/com/example/macqurarieinterns/StudentRegisterActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import static com.example.macqurarieinterns.Function.MyIntent.moveActivity;
 
@@ -13,7 +16,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class StudentRegisterActivity extends AppCompatActivity {
-
+    private EditText name,student_id, nic,email, phone,password1, password2;
+    private TextView dob;
+    private ImageButton date_dropdown;
+    private AutoCompleteTextView center;
+    private ImageButton center_dropdown;
+    private AutoCompleteTextView degree;
+    private ImageButton course_dropdown;
+    private TextView r_stud_year;
+    private ImageButton year_dropdown;
+    private AutoCompleteTextView intake;
+    private ImageButton batch_dropdown;
     private ImageButton back_btn, register_btn;
     private RelativeLayout login_btn;
 
@@ -22,6 +35,20 @@ public class StudentRegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_register);
 
+        name = findViewById(R.id.name);
+        student_id = findViewById(R.id.student_id);
+        nic = findViewById(R.id.nic);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
+        password1 = findViewById(R.id.password1);
+        password2 = findViewById(R.id.password2);
+        dob = findViewById(R.id.dob);
+        center = findViewById(R.id.center);
+        degree = findViewById(R.id.degree);
+        r_stud_year = findViewById(R.id.r_stud_year);
+        intake = findViewById(R.id.intake);
+
+        date_dropdown = findViewById(R.id.date_dropdown);
         back_btn = findViewById(R.id.back_btn);
         login_btn = findViewById(R.id.login_btn);
         register_btn = findViewById(R.id.register_btn);
