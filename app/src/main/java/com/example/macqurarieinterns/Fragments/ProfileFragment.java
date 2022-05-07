a6ab97ed326f32769bcbe87be0ad44f27e65eda0
hirusha2577 <56668123+hirusha2577@users.noreply.github.com>
2022-05-07 19:44:57 +0530

    #20 company profile and cover image upload

--- app/src/main/java/com/example/macqurarieinterns/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/macqurarieinterns/Fragments/ProfileFragment.java
@@ -1,16 +1,23 @@
 package com.example.macqurarieinterns.Fragments;
 
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
@@ -21,9 +28,32 @@ import androidx.fragment.app.Fragment;
 import com.example.macqurarieinterns.CompanyAboutActivity;
 import com.example.macqurarieinterns.CompanyEditProfileActivity;
 import com.example.macqurarieinterns.MainActivity;
+import com.example.macqurarieinterns.Model.Company;
 import com.example.macqurarieinterns.PostCreateActivity;
 import com.example.macqurarieinterns.R;
 import com.example.macqurarieinterns.StudentEditProfileActivity;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class ProfileFragment extends Fragment  {
@@ -31,7 +61,22 @@ public class ProfileFragment extends Fragment  {
     private LinearLayout tab_post, tab_about, post_add2;
     private RelativeLayout about_btn, edit_btn;
     private ImageButton moreBtn;
-    private CardView post_add1;
+    private CardView post_add1, p_image_change, c_image_change;
+    private TextView name;
+    private CircleImageView profile_image;
+    private ImageView cover_image;
+
+    private FirebaseUser firebaseUser;
+    private FirebaseAuth firebaseAuth;
+    private StorageReference storageReference11, storageReference12, storageReference21, storageReference22 ;
+    private static final int P_IMAGE_REQUEST = 1;
+    private static final int C_IMAGE_REQUEST = 2;
+    private Uri uri;
+    private StorageTask uploadTask;
+
+    private static String userType;
+
+    private DatabaseReference databaseReference, databaseReference2;
 
     public ProfileFragment() {
     }
@@ -51,6 +96,73 @@ public class ProfileFragment extends Fragment  {
         moreBtn = view.findViewById(R.id.moreBtn);
         post_add2 = view.findViewById(R.id.post_add2);
         post_add1 = view.findViewById(R.id.post_add1);
+        p_image_change = view.findViewById(R.id.p_image_change);
+        c_image_change = view.findViewById(R.id.c_image_change);
+
+        name = view.findViewById(R.id.name);
+        profile_image = view.findViewById(R.id.profile_image);
+        cover_image = view.findViewById(R.id.cover_image);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        storageReference11 = FirebaseStorage.getInstance().getReference("company_profile");
+        storageReference12 = FirebaseStorage.getInstance().getReference("company_cover");
+        storageReference21 = FirebaseStorage.getInstance().getReference("student_profile");
+        storageReference22 = FirebaseStorage.getInstance().getReference("student_cover");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Company").child(firebaseUser.getUid());
+        databaseReference2 = FirebaseDatabase.getInstance().getReference("Student").child(firebaseUser.getUid());
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null){
+                    userType = "company";
+                    Company company = dataSnapshot.getValue(Company.class);
+                    name.setText(company.getName());
+                    if (company.getP_imageURL().equals("default")){
+                        profile_image.setImageResource(R.mipmap.ic_launcher);
+                    }else {
+                        Picasso.get().load(company.getP_imageURL()).into(profile_image);
+                    }
+                    if(company.getC_imageURL().equals("default")){
+                        cover_image.setImageResource(R.mipmap.ic_launcher);
+                    }else {
+                        Picasso.get().load(company.getC_imageURL()).into(cover_image);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+        databaseReference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null){
+                    userType = "student";
+                    Company company = dataSnapshot.getValue(Company.class);
+                    name.setText(company.getName());
+                    if (company.getP_imageURL().equals("default")){
+                        profile_image.setImageResource(R.mipmap.ic_launcher);
+                    }else {
+                        Picasso.get().load(company.getP_imageURL()).into(profile_image);
+                    }
+                    if(company.getC_imageURL().equals("default")){
+                        cover_image.setImageResource(R.mipmap.ic_launcher);
+                    }else {
+                        Picasso.get().load(company.getC_imageURL()).into(cover_image);
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
 
 
 
@@ -58,7 +170,6 @@ public class ProfileFragment extends Fragment  {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), CompanyAboutActivity.class);
-//                intent.putExtra("company_id", u_id);
                 startActivity(intent);
             }
         });
@@ -67,7 +178,6 @@ public class ProfileFragment extends Fragment  {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), StudentEditProfileActivity.class);
-//                intent.putExtra("company_id", u_id);
                 startActivity(intent);
             }
         });
@@ -76,7 +186,6 @@ public class ProfileFragment extends Fragment  {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), PostCreateActivity.class);
-//                intent.putExtra("company_id", u_id);
                 startActivity(intent);
             }
         });
@@ -85,7 +194,6 @@ public class ProfileFragment extends Fragment  {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), PostCreateActivity.class);
-//                intent.putExtra("company_id", u_id);
                 startActivity(intent);
             }
         });
@@ -113,7 +221,6 @@ public class ProfileFragment extends Fragment  {
                                 startActivity(intent1);
                                 return true;
 
-
                         }
                         return false;
                     }
@@ -122,8 +229,177 @@ public class ProfileFragment extends Fragment  {
         });
 
 
+        p_image_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_P_image();
+            }
+        });
+
+        c_image_change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                open_C_image();
+            }
+        });
+
+
 
         return view;
+    }
+
+    public static String getUserType() {
+        String myString = userType;
+        return myString;
+    }
+
+    private void open_P_image() {
+
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, P_IMAGE_REQUEST);
+    }
+    private void open_C_image() {
+
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, C_IMAGE_REQUEST );
+    }
+
+    private String getFilExtension(Uri uri){
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap= MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private  void uploard_P_Image(){
+        final ProgressDialog pd =new ProgressDialog(getContext());
+        pd.setMessage("uploading");
+        pd.show();
+        if (uri != null){
+            if(userType.equals("company")) {
+                final StorageReference fileReference = storageReference11.child(System.currentTimeMillis()
+                        + "." + getFilExtension(uri));
+                uploadTask = fileReference.putFile(uri);
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        return fileReference.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri downlodUri = task.getResult();
+                            String mUri = downlodUri.toString();
+                            databaseReference = FirebaseDatabase.getInstance().getReference("Company").child(firebaseUser.getUid());
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("P_imageURL", mUri);
+                            databaseReference.updateChildren(map);
+                            pd.dismiss();
+                        } else {
+                            Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                            pd.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                });
+            }else{
+                //student p image uploard
+            }
+        }else {
+            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private  void uploard_C_Image(){
+        final ProgressDialog pd =new ProgressDialog(getContext());
+        pd.setMessage("uploading");
+        pd.show();
+
+        if (uri != null){
+            if(userType.equals("company")) {
+                final StorageReference fileReference = storageReference12.child(System.currentTimeMillis()
+                        + "." + getFilExtension(uri));
+
+                uploadTask = fileReference.putFile(uri);
+                uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+                        return fileReference.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+                            Uri downlodUri = task.getResult();
+                            String mUri = downlodUri.toString();
+
+                            databaseReference = FirebaseDatabase.getInstance().getReference("Company").child(firebaseUser.getUid());
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("C_imageURL", mUri);
+                            databaseReference.updateChildren(map);
+                            pd.dismiss();
+                        } else {
+                            Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                            pd.dismiss();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                });
+            }else{
+                // student cover image uploard
+            }
+        }else {
+            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == P_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null){
+            uri = data.getData();
+
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(), "Upload is progress", Toast.LENGTH_SHORT).show();
+            }else {
+                uploard_P_Image();
+
+            }
+        }
+        if (requestCode == C_IMAGE_REQUEST && resultCode == RESULT_OK
+                &&data !=null && data.getData() != null){
+            uri = data.getData();
+
+            if (uploadTask != null && uploadTask.isInProgress()){
+                Toast.makeText(getContext(), "Upload is progress", Toast.LENGTH_SHORT).show();
+            }else {
+                uploard_C_Image();
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/macqurarieinterns/Model/Company.java
@@ -0,0 +1,112 @@
+package com.example.macqurarieinterns.Model;
+
+public class Company {
+
+    private String id;
+    private String name;
+    private String register_no;
+    private String address;
+    private String P_imageURL;
+    private String email;
+    private String type;
+    private String phone;
+    private String C_imageURL;
+    private String status;
+
+    public Company(){
+
+    }
+
+    public Company(String id, String name, String register_no, String address, String p_imageURL, String email, String type, String phone, String c_imageURL, String status) {
+        this.id = id;
+        this.name = name;
+        this.register_no = register_no;
+        this.address = address;
+        P_imageURL = p_imageURL;
+        this.email = email;
+        this.type = type;
+        this.phone = phone;
+        C_imageURL = c_imageURL;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegister_no() {
+        return register_no;
+    }
+
+    public void setRegister_no(String register_no) {
+        this.register_no = register_no;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getP_imageURL() {
+        return P_imageURL;
+    }
+
+    public void setP_imageURL(String p_imageURL) {
+        P_imageURL = p_imageURL;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getC_imageURL() {
+        return C_imageURL;
+    }
+
+    public void setC_imageURL(String c_imageURL) {
+        C_imageURL = c_imageURL;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
