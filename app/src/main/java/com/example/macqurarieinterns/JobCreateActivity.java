b85dc364f515907b92a616525db30966ee2c3725
hirusha2577 <56668123+hirusha2577@users.noreply.github.com>
2022-05-08 11:15:07 +0530

    #36 publish vacancy

--- app/src/main/java/com/example/macqurarieinterns/JobCreateActivity.java
+++ app/src/main/java/com/example/macqurarieinterns/JobCreateActivity.java
@@ -4,15 +4,48 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.example.macqurarieinterns.Model.Company;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 import java.util.Objects;
 
+import static com.example.macqurarieinterns.Function.MyIntent.moveActivity;
+
 public class JobCreateActivity extends AppCompatActivity {
+
+    private EditText title,description;
+    private ImageButton field_dropdown;
+    private AutoCompleteTextView field;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference, databaseReference1;
+    private String companyName, companyId;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +63,39 @@ public class JobCreateActivity extends AppCompatActivity {
             }
         });
 
+        pd = new ProgressDialog(this);
+
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        field_dropdown = findViewById(R.id.field_dropdown);
+        field = findViewById(R.id.field);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser=firebaseAuth.getCurrentUser();
+        companyId = firebaseUser.getUid();
+
+        databaseReference1 = FirebaseDatabase.getInstance().getReference("Company").child(companyId).child("name");
+        databaseReference1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                companyName = (String) dataSnapshot.getValue();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        String[] types ={"C","C++","Java",".NET","iPhone","Android","ASP.NET","PHP"};
+        ArrayAdapter<String> companyTypes = new ArrayAdapter<String>(JobCreateActivity.this , android.R.layout.simple_dropdown_item_1line, types);
+        field.setAdapter(companyTypes);
+
+        field_dropdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                field.showDropDown();
+            }
+        });
+
     }
 
     @Override
@@ -42,9 +108,53 @@ public class JobCreateActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.add_post:
-                Toast.makeText(JobCreateActivity.this,"ok",Toast.LENGTH_LONG).show();
+                String txt_title = title.getText().toString();
+                String txt_field = field.getText().toString();
+                String txt_description = description.getText().toString();
+
+                if(TextUtils.isEmpty(txt_title) || TextUtils.isEmpty(txt_field) || TextUtils.isEmpty(txt_description)){
+                    Toast.makeText(JobCreateActivity.this, "All filed are required", Toast.LENGTH_SHORT).show();
+                }else{
+                    createVacancy(txt_title, txt_field, txt_description);
+                }
                 return true;
         }
         return false;
+    }
+
+    private void createVacancy(final String title, final String field, final String description){
+
+        pd.setMessage("Publishing Vacancy....");
+        pd.show();
+
+        final String timestamp = String.valueOf(System.currentTimeMillis());
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("id", companyId);
+        hashMap.put("company_name", companyName);
+        hashMap.put("title",title);
+        hashMap.put("field",field);
+        hashMap.put("description", description);
+        hashMap.put("pTime", timestamp);
+        hashMap.put("appliers_count", "0");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Vacancy");
+        databaseReference.child(timestamp).setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        pd.dismiss();
+                        Toast.makeText(JobCreateActivity.this, "Vacancy published", Toast.LENGTH_SHORT).show();
+                        moveActivity(JobCreateActivity.this,MainActivity.class);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        pd.dismiss();
+                        Toast.makeText(JobCreateActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
