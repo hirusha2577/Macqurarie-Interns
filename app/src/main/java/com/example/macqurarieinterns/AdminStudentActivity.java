e2cfa42f5733277971176fc06bc00f405bf9f3b1
uditha1998 <uditha.synotec@gmail.com>
2022-05-08 22:33:29 +0530

    #56 Admin-Student Activity

--- app/src/main/java/com/example/macqurarieinterns/AdminStudentActivity.java
+++ app/src/main/java/com/example/macqurarieinterns/AdminStudentActivity.java
@@ -3,14 +3,17 @@ package com.example.macqurarieinterns;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import java.util.Objects;
 
 import static com.example.macqurarieinterns.Function.MyIntent.moveActivity;
 
 public class AdminStudentActivity extends AppCompatActivity {
+    private ImageButton deactivateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,16 @@ public class AdminStudentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 moveActivity(AdminStudentActivity.this,AdminMainActivity.class);
+            }
+
+        });
+        deactivateBtn = findViewById(R.id.deactivateBtn);
+
+        deactivateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AdminStudentActivity.this, JobInterviewCreateActivity.class);
+                startActivity(intent);
             }
         });
     }
