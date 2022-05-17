acccfedfd72c98351825bb9fd8c1704fb8302d4a
uditha1998 <uditha.synotec@gmail.com>
2022-05-18 01:53:09 +0530

    #87 Missing Unstage Items of it20639112Thanu

--- app/src/main/java/com/example/macqurarieinterns/ReportStudentRegistrationActivity.java
+++ app/src/main/java/com/example/macqurarieinterns/ReportStudentRegistrationActivity.java
@@ -1,30 +1,16 @@
 package com.example.macqurarieinterns;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import java.util.Objects;
 
 import static com.example.macqurarieinterns.Function.MyIntent.moveActivity;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-
 public class ReportStudentRegistrationActivity extends AppCompatActivity {
-
-    private TextView available,active, percentage;
-    private long active_count, percentage_count;
-
-    String txt_active, txt_available;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,45 +25,6 @@ public class ReportStudentRegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 moveActivity(ReportStudentRegistrationActivity.this,AdminMainActivity.class);
-            }
-        });
-
-
-        available = findViewById(R.id.rejected);
-        active = findViewById(R.id.accepted);
-        percentage = findViewById(R.id.percentage);
-
-//      System.out.println("/////////////////////////////////////////////////////////////");
-//        System.out.println(txt_requested);
-//        System.out.println("/////////////////////////////////////////////////////////////");
-
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        databaseReference.child("Student").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                long count= dataSnapshot.getChildrenCount();
-                available.setText(Long.toString(count));
-                txt_available = Long.toString(count);
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        DatabaseReference databaseReference2 = FirebaseDatabase.getInstance().getReference("Student");
-        Query query1 = databaseReference2.orderByChild("status").equalTo("1");
-        query1.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                long count= dataSnapshot.getChildrenCount();
-                active.setText(Long.toString(count));
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
     }
--- app/src/main/java/com/example/macqurarieinterns/StudentApplyActivity.java
+++ app/src/main/java/com/example/macqurarieinterns/StudentApplyActivity.java
@@ -1,49 +1,19 @@
 package com.example.macqurarieinterns;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
-import com.example.macqurarieinterns.Adapter.ApplyAdapter;
-import com.example.macqurarieinterns.Model.Apply;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class StudentApplyActivity extends AppCompatActivity {
-
-    private FirebaseUser firebaseUser;
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference, databaseReference1;
-
-    RecyclerView recyclerView;
-    ApplyAdapter myAdapter;
-    ArrayList<Apply> applyList;
-    Context context;
-
-    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_apply);
-
-        Log.i("info", "start");
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -54,47 +24,6 @@ public class StudentApplyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        userId = firebaseUser.getUid();
-
-        context = getApplicationContext();
-
-        recyclerView = findViewById(R.id.applyList);
-//        database = FirebaseDatabase.getInstance().getReference(Clients.class.getSimpleName());
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        applyList = new ArrayList<>();
-        myAdapter = new ApplyAdapter(this, applyList);
-        recyclerView.setAdapter(myAdapter);
-
-        loadAllApply();
-
-    }
-
-    private void loadAllApply() {
-        Log.i("info", "Load apply start");
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Apply");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                applyList.clear();
-
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-                    Log.i("info", "in data snapshot");
-                    Apply apply = dataSnapshot.getValue(Apply.class);
-                    applyList.add(apply);
-
-                }
-                myAdapter.notifyDataSetChanged();
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(context, "" + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
