6276a8dff0fadfe2b9aa7203d35ba915e587f20b
uditha1998 <uditha.synotec@gmail.com>
2022-05-17 20:17:57 +0530

    #78 Admin-Create job Category

---
+++ app/src/main/java/com/example/macqurarieinterns/Model/JobCategory.java
@@ -0,0 +1,24 @@
+package com.example.macqurarieinterns.Model;
+
+public class JobCategory {
+    String category_id;
+    String name;
+
+    public String getCategory_id() {
+        return category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCategory_id(String category_id) {
+        this.category_id = category_id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
